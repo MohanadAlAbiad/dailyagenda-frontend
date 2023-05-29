6fddee77404fc1525fa2936ab43dac63c256a156
Mohanad Al-Abiad <weiss97m@gmail.com>
2023-05-29 15:38:44 +0200

    commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your DailyAgenda!"/>
+    <HelloWorld msg="Welcome to Your DailyAgenda!!!"/>
   </div>
 </template>
 
